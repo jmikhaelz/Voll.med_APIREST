aca9784bf68f4eba8385972b35cecb7b492200ed
z3nt3n0 <jmz06@outlook.com>
2025-07-16 18:40:14 -0600

    Create (model) : Creacion de class acerca sobre de Medical Json procesados a DTO

---
+++ src/main/java/med/voll/api/model/medical/DataMedical.java
@@ -0,0 +1,14 @@
+package med.voll.api.model.medical;
+
+import med.voll.api.model.DataAddress;
+/*
+ * DTO Registro de Medico/Doctor
+ */
+public record DataMedical(
+    String nombre,
+    String email,
+    String telefono,
+    String documento,
+    Specialty especialidad,
+    DataAddress direccion
+) {}
---
+++ src/main/java/med/voll/api/model/medical/Specialty.java
@@ -0,0 +1,10 @@
+package med.voll.api.model.medical;
+/*
+ * DTO Enumeracion/categorias de la especialidad de Doctor
+ */
+public enum Specialty {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DEMARTOLOGIA
+}
