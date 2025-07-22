ec0bd10549626b338ea6ab4fbacdbed5dd87df3b
z3nt3n0 <jmz06@outlook.com>
2025-07-22 14:53:04 -0600

    feat (model) : Adicion de metodos para la UserDetails

--- src/main/java/med/voll/api/model/person/User.java
+++ src/main/java/med/voll/api/model/person/User.java
@@ -47,4 +47,23 @@ public class User implements UserDetails {
         return login;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
