8879d5290abb50bc3c95ff70694468295cc424af
z3nt3n0 <jmz06@outlook.com>
2025-07-21 20:54:43 -0600

    Update (infra) : Class MannagerErrors para manejo de contexto de body

--- src/main/java/med/voll/api/infra/MannagerErrors.java
+++ src/main/java/med/voll/api/infra/MannagerErrors.java
@@ -1,12 +1,18 @@
 package med.voll.api.infra;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.persistence.EntityNotFoundException;
+
 /*
- * Class para manejo de errores para direccionar 404 Not found
+ * Class para manejo de errores
+ * Simplificar el JSON devuelto por la API en casos de error de validación de Bean Validation.
  */
 @RestControllerAdvice
 public class MannagerErrors {
@@ -14,5 +20,21 @@ public class MannagerErrors {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> errorHandler404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DataErrorValid>> errorHandlerBlank(MethodArgumentNotValidException ex) {
+        var errores = ex.getFieldErrors().stream()
+                .map(DataErrorValid::new)
+                .toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    public record DataErrorValid(
+            String campo,
+            String mensaje) {
+        public DataErrorValid(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
