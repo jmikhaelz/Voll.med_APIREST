56109bb5a38fc0d04ef6edcb45795c46c9a6b42b
z3nt3n0 <jmz06@outlook.com>
2025-07-22 14:49:45 -0600

    create (controller) : Control de autentificacion de session para API

---
+++ src/main/java/med/voll/api/controller/UserController.java
@@ -0,0 +1,30 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import med.voll.api.model.person.DataAutowired;
+
+@RestController
+@RequestMapping("/login")
+public class UserController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity<Object> initSession(@RequestBody @Valid DataAutowired data) {
+        var token = new UsernamePasswordAuthenticationToken(
+                data.login(),
+                data.psw());
+        var authentication = manager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}
