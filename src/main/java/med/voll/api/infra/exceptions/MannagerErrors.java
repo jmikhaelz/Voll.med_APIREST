57c730de4efe58bf01e3b8c2485481cee6c69999
z3nt3n0 <jmz06@outlook.com>
2025-07-23 14:11:12 -0600

    feat (controller) : Manejo de errores de request HTTP

--- src/main/java/med/voll/api/infra/exceptions/MannagerErrors.java
+++ src/main/java/med/voll/api/infra/exceptions/MannagerErrors.java
@@ -2,7 +2,12 @@ package med.voll.api.infra.exceptions;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,6 +33,31 @@ public class MannagerErrors {
                 .map(DataErrorValid::new)
                 .toList();
         return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> gestionarError400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> gestionarErrorBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Object> gestionarErrorAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falla en la autenticación");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> gestionarErrorAccesoDenegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acceso denegado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> gestionarError500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + ex.getLocalizedMessage());
     }
 
     public record DataErrorValid(
