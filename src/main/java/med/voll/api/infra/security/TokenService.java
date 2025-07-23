10ca50868e1ad3d8e462ff9278bd1f6d4e4ef058
z3nt3n0 <jmz06@outlook.com>
2025-07-22 21:16:54 -0600

    create (infra) : Creacion de instacia y generador de Token

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,42 @@
+package med.voll.api.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import med.voll.api.model.person.User;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Value("${api.security.token.author}")
+    private String author;
+
+    public String createToken(User data) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer(author)
+                    .withSubject(data.getUsername())
+                    .withExpiresAt(fechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error al generar el Token JWT");
+        }
+    }
+
+    private Instant fechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-06:00"));
+    }
+
+}
